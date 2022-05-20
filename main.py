94e14f8ac23f35d05b1c2f4760146bc1c5b21db7
Kolya <kolya-sob@yandex.ru>
2022-05-20 08:11:45 +0300

    Структура файлов бота.

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
-# -*- coding: utf-8 -*-
-
+import os
 import telebot
-from telebot import types
-import settings
 
-try:
-    from local_config import TOKEN
-except ImportError:
-    exit('Do copy paste local_config.py.default (local_config.py) and set TOKEN')
+BOT_NAME = os.getenv('BOT_USERNAME')
+TOKEN = os.getenv('BOT_TOKEN')
 
 bot = telebot.TeleBot(TOKEN)
 
 
-@bot.message_handler(commands=list(settings.COMMAND))
-def command_message(message):
-    command = message.text[1:]
-    bot.send_message(message.chat.id, settings.COMMAND[command]['answer'])
+@bot.message_handler(commands=['start'])
+def cmd_start(message):
+    bot.send_message(message.chat.id, 'Привет! Вы запустили бота, который производит поиск отлей.\n'
+                                      'Ознакомься с инструкция по работе с ним - /help.')
 
 
-@bot.message_handler(func=lambda message: True, content_types=['audio', 'photo', 'voice', 'video', 'document',
-                                                               'text', 'location', 'contact', 'sticker'])
-def error_message(message):
-    bot.send_message(message.chat.id, settings.DEFAULT_MESSAGE)
+@bot.message_handler(commands=['help'])
+def cmd_start(message):
+    bot.send_message(message.chat.id, 'Инструкция по работе бота.\n'
+                                      '/lowprice - узнать топ самых дешёвых отелей в городе\n'
+                                      '/highprice - узнать топ самых дорогих отелей в городе\n'
+                                      '/bestdeal - вывод отелей, наиболее подходящих по цене и расположению от центра\n'
+                                      '/history - вывод истории поиска отелей')
 
 
-bot.infinity_polling()
+if __name__ == '__main__':
+    bot.infinity_polling()
