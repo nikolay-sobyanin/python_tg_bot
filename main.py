231adb8701ae85133fe699c50d2a747b8ddc842e
Nikolay <kolya-sob@yandex.ru>
2022-05-29 12:33:56 +0300

    Готова структура проекта. Создан модуль db_worker.py для работы с DB.

--- main.py
+++ main.py
@@ -1,25 +1,58 @@
-import os
 import telebot
+import config
+from data_base.db_worker import ControlState
 
-BOT_NAME = os.getenv('BOT_USERNAME')
-TOKEN = os.getenv('BOT_TOKEN')
+bot = telebot.TeleBot(config.BOT_TOKEN)
+state = ControlState(config.PATH_DB)
 
-bot = telebot.TeleBot(TOKEN)
+list_commands = ['start', 'help', 'lowprice', 'highprice', 'bestdeal', 'history', 'reset']
+
+
+@bot.message_handler(commands=['reset'])
+def reset_state(message):
+    bot.send_message(message.chat.id, 'Сброс состояния')
+
+
+@bot.message_handler(commands=list_commands, func=lambda x: True)
+def check_state(message):
+    bot.send_message(message.chat.id, 'Проверка состояния')
 
 
 @bot.message_handler(commands=['start'])
 def cmd_start(message):
-    bot.send_message(message.chat.id, 'Привет! Вы запустили бота, который производит поиск отлей.\n'
-                                      'Ознакомься с инструкция по работе с ним - /help.')
+    bot.send_message(message.chat.id, 'Привет! Ты запустили бота, который производит поиск отлей.\n'
+                                      'Ознакомься с его командами - /help.')
 
 
 @bot.message_handler(commands=['help'])
-def cmd_start(message):
+def cmd_help(message):
     bot.send_message(message.chat.id, 'Инструкция по работе бота.\n'
-                                      '/lowprice - узнать топ самых дешёвых отелей в городе\n'
-                                      '/highprice - узнать топ самых дорогих отелей в городе\n'
-                                      '/bestdeal - вывод отелей, наиболее подходящих по цене и расположению от центра\n'
-                                      '/history - вывод истории поиска отелей')
+                                      '/lowprice - поиск топ самых дешёвых отелей в городе\n'
+                                      '/highprice - поиск топ самых дорогих отелей в городе\n'
+                                      '/bestdeal - поиск отелей наиболее подходящих по цене и расположению от центра\n'
+                                      '/history - вывод истории поиска отелей\n'
+                                      '/reset - сброс текущего поиска')
+
+
+@bot.message_handler(commands=['lowprice'])
+def cmd_lowprice(message):
+    state.set_state(message.chat.id, 'lowprice', 'step_1')
+    bot.send_message(message.chat.id, 'Ты выбрал команду lowprice')
+
+
+@bot.message_handler(commands=['highprice'])
+def cmd_highprice(message):
+    bot.send_message(message.chat.id, 'Ты выбрал команду highprice')
+
+
+@bot.message_handler(commands=['bestdeal'])
+def cmd_bestdeal(message):
+    bot.send_message(message.chat.id, 'Ты выбрал команду bestdeal')
+
+
+@bot.message_handler(commands=['history'])
+def cmd_history(message):
+    bot.send_message(message.chat.id, 'Ты выбрал команду bestdeal')
 
 
 if __name__ == '__main__':
